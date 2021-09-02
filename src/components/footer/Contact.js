fd30927a070ae8967eb8f9f070c9ce417827ef75
himanshu_kumar <kumarhimanshu0620@gmail.com>
2021-09-02 11:25:38 +0530

    himanshu updated

--- src/components/footer/Contact.js
+++ src/components/footer/Contact.js
@@ -9,28 +9,28 @@ const Contact = () => {
                     <div className="row gy-4">
                         <div className="col-md-6">
                             <div className="info-box text-center" style={{backgroundColor: "rgb(225, 239, 240)"}}>
-                                <i className="bi bi-geo-alt" style={{color:"blue"}}></i>
+                                <i className="bi bi-geo-alt" style={{color:"blue",fontSize:"25px"}}></i>
                                 <h3 style={{color: "#012970"}}>Address</h3>
                                 <p>Burdwan<br />West Bengal,713104<br/>India</p>
                             </div>
                         </div>
                         <div className="col-md-6 ">
                             <div className="info-box text-center" style={{backgroundColor: "rgb(225, 239, 240)"}}>
-                                <i className="bi bi-telephone" style={{color:"blue"}}></i>
+                                <i className="bi bi-telephone" style={{color:"blue",fontSize:"25px"}}></i>
                                 <h3 style={{color: "#012970"}}>Call Us</h3>
                                 <p>+91 9110065062<br />+91 9801178859<br />+91 9801178859</p>
                             </div>
                         </div>
                         <div className="col-md-6">
                             <div className="info-box text-center" style={{backgroundColor: "rgb(225, 239, 240)"}}>
-                                <i className="bi bi-envelope" style={{color:"blue"}}></i>
+                                <i className="bi bi-envelope" style={{color:"blue",fontSize:"25px"}}></i>
                                 <h3 style={{color: "#012970"}}>Email Us</h3>
                                 <p>kumarhimanshu0620@gmail.com<br />afhamfardeen98@gmail.com<br />chaturcr94@gmail.com</p>
                             </div>
                         </div>
                         <div className="col-md-6">
                             <div className="info-box text-center" style={{backgroundColor: "rgb(225, 239, 240)"}}>
-                                <i className="bi bi-clock" style={{color:"blue"}}></i>
+                                <i className="bi bi-clock" style={{color:"blue",fontSize:"25px"}}></i>
                                 <h3 style={{color: "#012970"}}>Open Hours</h3>
                                 <p>Monday - Friday<br />9:00AM - 05:00PM<br/><br/></p>
                             </div>
@@ -39,9 +39,9 @@ const Contact = () => {
 
                 </div>
 
-                <div className="col-lg-6" style={{backgroundColor: "rgb(225, 239, 240)"}}><br/>
+                <div className="col-lg-6" style={{backgroundColor: "rgb(225, 239, 240)",height:"342px"}}>
                     <form>
-                        <div className="row gy-4">
+                        <div className="row gy-4 pt-1">
 
                             <div className="col-md-6">
                                 <input type="text" name="name" className="form-control" placeholder="Your Name" required="" />
@@ -57,21 +57,15 @@ const Contact = () => {
 
                             <div className="col-md-12">
                                 <textarea className="form-control" name="message" rows="6" placeholder="Message" required=""></textarea>
+                                
                             </div>
-
                             <div className="text-center">
-                                <button type="submit" className="btn btn-primary btn-lg">Send Message</button>
-                            </div>
-
-
-
-
-                        </div><br />
-
+                            <button type="submit" className="btn btn-primary btn-sm">Send Message</button>
+                             </div>   
+                            
+                        </div>
                     </form>
-
                 </div>
-
             </div>
         </>
     )

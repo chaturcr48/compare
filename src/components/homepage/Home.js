4f5eee107305b12fdb56b8e5615a185f9fa59c48
Afham Fardeen <49563140+afham123@users.noreply.github.com>
2021-09-26 13:00:39 +0530

    Update Home.js

--- src/components/homepage/Home.js
+++ src/components/homepage/Home.js
@@ -7,27 +7,10 @@ import PopularMobiles from "./PopularMobiles";
 import PopulatTablets from "./PopularTablets";
 // import PopularTelevision from "./PopularTelevision";
 
-import { Helmet } from "react-helmet";
-
 const Home = () => {
   return (
     <React.Fragment>
       <MainCarousal />
-      <Helmet>
-        <script
-          type="text/javascript"
-          src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js"
-          data-name="bmc-button"
-          data-slug="chatur"
-          data-color="#FFDD00"
-          data-emoji="🍕"
-          data-font="Cookie"
-          data-text="Buy me a pizza"
-          data-outline-color="#000000"
-          data-font-color="#000000"
-          data-coffee-color="#ffffff"
-        ></script>
-      </Helmet>
       {/* <MobileSlider /> */}
       <LatestMobile />
       <PopularMobiles />
